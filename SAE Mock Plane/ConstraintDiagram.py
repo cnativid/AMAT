6c66cbae3dd0faa5a79b301744113f9c46d3ecbe
GorgonzolaTortilla <133760067+GorgonzolaTortilla@users.noreply.github.com>
2023-05-22 20:58:54 -0700

    brain damage changes
    removed V_stall from LDG and did V_stall separately inside the Cruise constraint

--- SAE Mock Plane/ConstraintDiagram.py
+++ SAE Mock Plane/ConstraintDiagram.py
@@ -6,7 +6,7 @@ from labellines import labelLines # pip install matplotlib-label-lines
 from ambiance import Atmosphere
 
 g = 32.17 # ft/s^2
-
+rho = 0.0023769 # sea level, slug/ft^3
 
 def Constraint_TO(WS):
     eta_p = 0.45 # Gudmunsson, 
@@ -15,14 +15,11 @@ def Constraint_TO(WS):
     C_DTO =  0.02 
     C_Lmax = 1.5
     mu = 0.03
-    rho = 0.0023769 # sea level, slug/ft^3
     V_LOF = 1.1*np.sqrt(2*WS/(rho*C_Lmax))
     return 1/((1.21/(g*rho*C_Lmax*S_G)*WS+0.605/C_Lmax*(C_DTO-mu*C_LTO) + mu)*V_LOF/np.sqrt(2)/(eta_p*550/745.7))
 
-def Constraint_LDG():
-    S_LGR = 200.0 # ft
+def Constraint_LDG(S_LGR = 200):
     mu = 0.3
-    rho = 0.0023769 # sea level, slug/ft^3
     C_LLD =  0
     C_DLD =  0.1
     C_Lmax = 1.5
@@ -57,8 +54,8 @@ def Constraint_LDG():
         res = [x1[-1][0]-S_LGR,x2[-1][0]-S_LGR]
         V_stall = [V_stall[1] , V_stall[1] - res[1]/((res[1]-res[0])/(V_stall[1]-V_stall[0]))]
     V_stall = V_stall[0]
-    return [rho*V_stall**2*C_Lmax/2,V_stall]
-    # return S_LGR/80*C_Lmax
+    return rho*V_stall**2*C_Lmax/2
+                # return S_LGR/80*C_Lmax
 
 # def Constraint_ngTurn(WS,n):
 #     C_Lmax = 1
@@ -77,13 +74,14 @@ def Constraint_MaxWeight():
     W_max = 55 # MTOW, lb
     return W_max/P_max
 
-def Constraint_Cruise(WS,V_cruise):
-    eta_p = 0.8 # prop efficiency at trim
+def Constraint_Cruise(WS,V_variation):
+    eta_p = 0.65 # prop efficiency at trim
+    C_Lmax = 1.5
     P_cruise = 0.5 # frac of max power for cruise
-    # V_cruise = 30
+    V_stall = np.sqrt(2*WS/rho/C_Lmax)
+    V_cruise = V_stall + V_variation
     # LD = 9 # trim L/D
     C_D0 = 0.02
-    rho = 0.0023769 # sea level, slug/ft^3
     q  = rho/2*V_cruise**2
     AR = 10
     e = .8
@@ -97,32 +95,28 @@ if __name__ == "__main__":
 
     ## Evaluate Constraints
     WP_Takeoff=Constraint_TO(WS)
-    LDG_values = Constraint_LDG() # [ WS_Landing , V_stall ]
+    WS_Landing = Constraint_LDG(200)*np.ones(N)
     # WS_Landing=Constraint_LDG()*np.ones(N)
-    WS_Landing=LDG_values[0]*np.ones(N)
     WP_MaxWeight=Constraint_MaxWeight()*np.ones(N)
     # WP_Cruise30=Constraint_Cruise(WS,30)
     # WP_Cruise50=Constraint_Cruise(WS,50)
-    V_variation = [10,20,30] # Difference between cruise velocity and stall velocity
-    V_Cruise = LDG_values[1]*np.ones(len(V_variation)) + V_variation
-    WP_Cruise = np.zeros([len(V_Cruise),len(WS)])
-    for i in range(0,len(V_Cruise)):
-        WP_Cruise[i,:] = Constraint_Cruise(WS,V_Cruise[i]) # Cruise @ V_stall + 10
+    V_variation = [0,10,20,30,40,50] # Difference between cruise velocity and stall velocity
+    # V_Cruise = LDG_values[1]*np.ones(len(V_variation)) + V_variation
+    WP_Cruise = np.zeros([len(V_variation),len(WS)])
+    for i in range(0,len(V_variation)):
+        WP_Cruise[i,:] = Constraint_Cruise(WS,V_variation[i]) # Cruise @ V_stall + V_variation
 
 
     ## Plot constraints
     plt.plot(WS,Constraint_TO(WS), label = '100 ft Takeoff')
-    plt.plot(WS_Landing,WP, label = '400 ft Takeoff')
+    plt.plot(WS_Landing,WP, label = '200 ft Landing')
     plt.plot(WS,WP_MaxWeight, label = 'Max Weight (55 lb)')
-    # plt.plot(WS,WP_Cruise30, label = '30 ft/s Cruise')
-    # plt.plot(WS,WP_Cruise50, label = '50 ft/s Cruise')
-    for i in range(0,len(V_Cruise)):
+    for i in range(0,len(V_variation)):
         plt.plot(WS,WP_Cruise[i,:], label = f'V_stall + {V_variation[i]} ft/s Cruise')
 
     ## Plotting Options
     plt.xlabel('W/S [lbf/ft^2]')
     plt.ylabel('W/P [lbf/W]')
-    # plt.legend()
     plt.ylim(0,0.1)
     plt.grid()
     plt.minorticks_on()
